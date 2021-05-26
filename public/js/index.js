d8d97d154037797ee79b8d9b085989e4ef096a59
Pranjal <71340514+roverboy773@users.noreply.github.com>
2021-05-26 15:24:29 +0530

    Env added

--- public/js/index.js
+++ public/js/index.js
@@ -234,7 +234,7 @@ else if (window.location.pathname === '/cart') {
             const { amount, id: order_id, currency } = rslt.data
 
             const options = {
-                key: "rzp_test_mTGKc1dmNTnXYK", // Enter the Key ID generated from the Dashboard
+                key: process.env.RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
                 image: '/img/pizza_svg.svg',
                 currency: currency,
                 name: "PeppiPerry-Pizza House",
